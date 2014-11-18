2d562028d15a06928f0afc3180fb7ce21fab83cb
mongkuen <mongkuen.sun@gmail.com>
2014-11-18 00:26:23 +0000

    Static Pages Controller

---
+++ radial_review/app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def info
+  end
+
+  def privacy
+  end
+
+  def thankyou
+  end
+
+  def tos
+  end
+end
--- radial_review/config/routes.rb
+++ radial_review/config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/info'
+
+  get 'static_pages/privacy'
+
+  get 'static_pages/thankyou'
+
+  get 'static_pages/tos'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
